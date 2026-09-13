c19d11ca3821078fd0b99f7e06f55c7b1bdc7feb
sahip <sahipsahip99@gmail.com>
2026-09-13 23:09:29 +0530

    Add PWA manifest and Service Worker for Add to Home Screen mobile installation

---
+++ frontend/public/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'zahi-cache-v1';
+const urlsToCache = ['/', '/index.html', '/favicon.svg'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
